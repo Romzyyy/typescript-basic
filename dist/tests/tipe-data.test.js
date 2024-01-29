94eeeb2e8a557487b72e9e1807ec58d4d23c4942
romzyyy <fijay0117@gmail.com>
2024-01-29 21:08:59 +0700

    learn data type

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('data type', () => {
+    it('sould be declare', () => {
+        const name = "eko kurniawan khanedy";
+        const balance = 100000;
+        const isVip = true;
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('data type', () => { 
+    it('sould be declare', () => {
+        const name: string = "eko kurniawan khanedy"
+        const balance: number = 100000
+        const isVip: boolean = true
+
+        console.log(name)
+        console.log(balance)
+        console.log(isVip)
+    })
+ })
